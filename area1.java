9a63f8c98c7f5a888232cfb0167c8acc0ee707ea
AksSin2003 <akshitsinha@ymail.com>
2016-06-22 03:20:16 +0000

    Area of rectangle

---
+++ area1.java
@@ -0,0 +1,18 @@
+
+public class area1 extends rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		area1 obj=new area1();
+		obj.breadth=11;
+		obj.length=10;
+		obj.area1();	
+		
+		rectangle obj1=new rectangle(11,13);
+		obj1.area1();
+		
+
+	}
+
+}
